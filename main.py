c561075269f7ccbf50fb9f9da5f6fdb2c7851490
peprolinbot <personal@peprolinbot.com>
2023-02-15 16:13:58 +0100

    Add support for services

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ docker_client = docker.from_env()
 
 app = Flask(__name__)
 
+
 @app.route('/container/<id_or_name>')
 def get_container_status(id_or_name):
     try:
@@ -26,6 +27,22 @@ def get_container_status(id_or_name):
             status_code = UNAVALIABLE_STAUS_CODE
     return output, status_code
 
+
+@app.route('/service/<id_or_name>')
+def get_service_status(id_or_name):
+    try:
+        service = docker_client.services.get(id_or_name)
+    except docker.errors.NotFound:
+        output = {"error": "Service not found"}
+        status_code = NOT_FOUND_STATUS_CODE
+    else:
+        status_code = AVALIABLE_STAUS_CODE
+        output = {"status": {}}
+        for i, task in enumerate(service.tasks()):
+            output["status"][i] = status = task["Status"]["State"]
+            if status != "running":
+                status_code = UNAVALIABLE_STAUS_CODE
+    return output, status_code
 
 
 if __name__ == '__main__':
